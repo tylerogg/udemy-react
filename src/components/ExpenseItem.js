52c5fbe384bf5eca60dc2d3dd7999a69cbff0f57
Tyler Ogg <tco9421@gmail.com>
2022-10-28 23:03:36 -0400

    Section 3 36.

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import './ExpenseItem.css'
+
+function ExpenseItem () {
+    return (
+        <div className="expense-item">
+            <div className="expense-item__description">March 28th 2021</div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div className="expense-item__price">$294.67</div>
+            </div>
+        </div>
+    );
+}
+
+    export default ExpenseItem;
